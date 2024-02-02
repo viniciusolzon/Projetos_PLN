64771f5e34b77a16d8131d6ffe9b22a517fd219e
Vinicius Freitas <91697213+viniciusolzon@users.noreply.github.com>
2024-02-02 19:10:35 -0300

    atividade finalizada

--- trigrama_bigrama.py
+++ trigrama_bigrama.py
@@ -1,9 +1,10 @@
+from collections import defaultdict, Counter
+from nltk.util import ngrams
 import re
-from collections import Counter
 
 regex = "[a-zA-ZçÇãÃõÕáÁéÉíÍóÓúÚâÂêÊîÎôÔûÛàÀ]+"
 
-data = open("alice.txt").read()
+data = open("alice.txt").read().lower()
 
 tokens = re.findall(regex, data)
 tokens_count = Counter(tokens)
@@ -20,20 +21,28 @@ def p_bigram(w1, w2):
     return count_w1w2/count_w1
 
 # Probabilidade de um trigrama
-def p_trigram(w1, w2, w3):
+def p_trigrama(trigrama):
     count_w1w2 = 0
     count_w1w2w3 = 0
 
     for i in range(len(tokens)-1):
-        if tokens[i] == w1 and tokens[i+1] == w2:
+        if tokens[i] == trigrama[0] and tokens[i+1] == trigrama[1]:
             count_w1w2 += 1
 
-    for i in range(len(tokens)-1):
-        if tokens[i] == w1 and tokens[i+1] == w2 and tokens[i+2] == w3:
+    for i in range(len(tokens)-2):
+        if tokens[i] == trigrama[0] and tokens[i+1] == trigrama[1] and tokens[i+2] == trigrama[2]:
             count_w1w2w3 += 1
-    
-    # print(count_w1w2)
-    # print(count_w1w2w3)
+
     return count_w1w2w3/count_w1w2
 
-print(p_trigram("Alice", "was", "not"))
+# Obtém todos os trigramas da "base de dados" 
+trigramas = list(ngrams(tokens, 3))
+
+# Calcula probabilidade de todos os trigramas e guarda em um dicionário
+prob_dict = {}
+for i in range(len(trigramas)):
+    prob_dict[trigramas[i]] = p_trigrama(trigramas[i])
+
+print(prob_dict)
+#country, capital = random.choice(list(d.items()))
+#capital = random.choice(list(d.values()))
